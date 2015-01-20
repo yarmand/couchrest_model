9fdeae50923a0d8a7e0b3ef65d32df55de0cbcd7
yann ARMAND <yarmand@yammer-inc.com>
2015-01-20 18:59:10 +0000

    [back_assignments] finish spec cases enumeration

--- spec/unit/assocations_dual_spec.rb
+++ spec/unit/assocations_dual_spec.rb
@@ -3,19 +3,30 @@ require 'spec_helper'
 
 describe 'Associations' do
 
-  describe 'of type belongs' do
-    context 'the other side also belongs_to' do
+  describe 'of type belongs_to' do
+    context 'the other side also belongs_to (1-1)' do
       it 'should set the other side property too' do
       end
     end
 
-    context 'the other side do not share association' do
+    context 'the other side do not back associate (1-0)' do
       it 'should set property without error' do
       end
     end
 
-    context 'the other side associate as a collection' do
+    context 'the other side associate as a collection (1-n)' do
       it 'should be part of the collection weh setting the property' do
+      end
+    end
+  end
+
+  descibe 'of type collection_of' do
+    context 'the other side is a belongs_to (n-1)' do
+      it 'should populate the belongs_to property when added to the collection'
+    end
+
+    context 'the other side do not back associate (n-0)' do
+      it 'should set property without error' do
       end
     end
   end
