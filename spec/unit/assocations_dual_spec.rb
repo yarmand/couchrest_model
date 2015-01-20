0bdf3f12c76ffa062d49c084d00721731b00b8b7
yann ARMAND <yarmand@yammer-inc.com>
2015-01-20 21:38:29 +0000

    [back_assignments] make examples 'not implemented yet'

--- spec/unit/assocations_dual_spec.rb
+++ spec/unit/assocations_dual_spec.rb
@@ -5,29 +5,26 @@ describe 'Associations' do
 
   describe 'of type belongs_to' do
     context 'the other side also belongs_to (1-1)' do
-      it 'should set the other side property too' do
-      end
+      it 'should set the other side property too'
     end
 
     context 'the other side do not back associate (1-0)' do
-      it 'should set property without error' do
-      end
+      it 'should set property without error'
+
     end
 
     context 'the other side associate as a collection (1-n)' do
-      it 'should be part of the collection weh setting the property' do
-      end
+      it 'should be part of the collection weh setting the property'
     end
   end
 
-  descibe 'of type collection_of' do
+  describe 'of type collection_of' do
     context 'the other side is a belongs_to (n-1)' do
       it 'should populate the belongs_to property when added to the collection'
     end
 
     context 'the other side do not back associate (n-0)' do
-      it 'should set property without error' do
-      end
+      it 'should set property without error'
     end
   end
 end
